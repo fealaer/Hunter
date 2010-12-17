6848f4ac69e8c5bb2d568bf283093fa745f7bf10
fealaer <fealaer@gmail.com>
2010-12-17 09:56:53 +0300

    first commit

---
+++ src/giftshunter/GiftsHunter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package giftshunter;
+
+/**
+ *
+ * @author fealaer
+ */
+public class GiftsHunter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
